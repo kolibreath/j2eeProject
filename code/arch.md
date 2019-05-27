f78d60f94f0f2403e306f11bb8d9f625fd809401
kolibreath <734780178@qq.com>
2019-05-27 09:16:15 +0800

    [ADD] 解决方法

--- code/arch.md
+++ code/arch.md
@@ -11,3 +11,13 @@ cron是linux上一个定时任务工具
 使用Idea 构建Spring boot 项目
 Spring initializr -> 勾选项目 web ，template engines 中选择 thymeleaf ，sql 选择jpa，然后选择创建pom.xml
 参考这个[项目](http://tengj.top/2017/02/26/springboot1/)创建一个spring boot项目
+
+- 出现呢 If you want an embedded database (H2, HSQL or Derby), please put it on the classpath.
+解决方法
+在Application 类上加上注解：
+```
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+```
+
+#Spring boot 链接mysql
+Springboot 中我们使用的是jpa 去链接mysql 数据库
--- code/j2eefinal/src/main/java/com/kolibreath/j2eefinal/J2eefinalApplication.java
+++ code/j2eefinal/src/main/java/com/kolibreath/j2eefinal/J2eefinalApplication.java
@@ -2,10 +2,18 @@ package com.kolibreath.j2eefinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class J2eefinalApplication {
 
+	@RequestMapping("/")
+	public String index(){
+		return "fuck you ";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(J2eefinalApplication.class, args);
 	}
